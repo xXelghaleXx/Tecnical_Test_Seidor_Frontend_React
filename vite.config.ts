bbab71e66185e7ca7216964d276facfa7e8e83e0
xXelghaleXx <adriannuno56@gmail.com>
2026-02-15 23:12:37 -0500

    despliegue con vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tecnical_Test_Seidor_Frontend_React/',
+  base: '/',
 })
